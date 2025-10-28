3d7dfa5fc329c523815d6a5fc4179ba89ddff340
DimitarMalamski <mitemalamata@gmail.com>
2025-10-28 15:47:03 +0100

    removed unneeded h1 elemenet from the GEO apge

--- frontend/src/pages/Geo.jsx
+++ frontend/src/pages/Geo.jsx
@@ -3,7 +3,6 @@ import DashboardMap from "../components/GEO/DashboardMap.jsx";
 export default function Geo() {
     return (
         <div className="p-4">
-            <h2 className="text-xl font-semibold mb-4">Sales Heatmap</h2>
             <DashboardMap />
         </div>
     );
