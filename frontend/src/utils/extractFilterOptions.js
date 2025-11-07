5455908e4ce95ec0604aa9d7118267d55c55ce8b
DimitarMalamski <mitemalamata@gmail.com>
2025-11-07 01:46:52 +0100

    Adding reset button to the filters

--- frontend/src/utils/extractFilterOptions.js
+++ frontend/src/utils/extractFilterOptions.js
@@ -1,7 +1,7 @@
 export function extractFilterOptions(offers) {
-    const statuses = [...new Set(offers.map(o => o.status))];
-    const salespeople = [...new Set(offers.map(o => o.salesperson))];
-    const depots = [...new Set(offers.map(o => o.depot))];
-
-    return { statuses, salespeople, depots };
+    return {
+        statuses: [...new Set(offers.map(o => o.status).filter(Boolean))],
+        salespeople: [...new Set(offers.map(o => o.salesperson).filter(Boolean))],
+        depots: [...new Set(offers.map(o => o.depot).filter(Boolean))],
+    };
 }
