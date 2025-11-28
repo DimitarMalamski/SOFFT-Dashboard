1e59315b99cfd5d531179f2a83fa0459303cafcf
DustVeer <dveerdonk@gmail.com>
2025-11-28 11:10:34 +0100

    Added env variables

--- backend/src/main/java/nl/fontys/s3/my_app/Services/CustomerCompanyService.java
+++ backend/src/main/java/nl/fontys/s3/my_app/Services/CustomerCompanyService.java
@@ -15,46 +15,46 @@ import nl.fontys.s3.my_app.models.dtos.CustomerCompany.CustomerCompanyDTO;
 @Service
 public class CustomerCompanyService {
 
-    private final CompanyAddressRepo companyAddressRepo;
-    private final CompanyPersonRepo companyPersonRepo;
-    private final CustomerCompanyRepo customerCompanyRepo;
+        private final CompanyAddressRepo companyAddressRepo;
+        private final CompanyPersonRepo companyPersonRepo;
+        private final CustomerCompanyRepo customerCompanyRepo;
 
-    public CustomerCompanyService(CompanyAddressRepo companyAddressRepo, CompanyPersonRepo companyPersonRepo,
-            CustomerCompanyRepo customerCompanyRepo) {
-        this.companyAddressRepo = companyAddressRepo;
-        this.companyPersonRepo = companyPersonRepo;
-        this.customerCompanyRepo = customerCompanyRepo;
-    }
+        public CustomerCompanyService(CompanyAddressRepo companyAddressRepo, CompanyPersonRepo companyPersonRepo,
+                        CustomerCompanyRepo customerCompanyRepo) {
+                this.companyAddressRepo = companyAddressRepo;
+                this.companyPersonRepo = companyPersonRepo;
+                this.customerCompanyRepo = customerCompanyRepo;
+        }
 
-    public List<CustomerCompanyDTO> getAll() {
-        return customerCompanyRepo.findAll()
-                .stream().map(cc -> new CustomerCompanyDTO(
-                        cc,
-                        companyAddressRepo.findByCompanyUuid(cc.getUuid())
-                                .stream()
-                                .map(CompanyAddressDTO::fromEntity)
-                                .collect(Collectors.toList()),
+        public List<CustomerCompanyDTO> getAll() {
+                return customerCompanyRepo.findAll()
+                                .stream().map(cc -> new CustomerCompanyDTO(
+                                                cc,
+                                                companyAddressRepo.findByCompanyUuid(cc.getUuid())
+                                                                .stream()
+                                                                .map(CompanyAddressDTO::fromEntity)
+                                                                .collect(Collectors.toList()),
 
-                        companyPersonRepo.findByCompanyUuid(cc.getUuid())
-                                .stream()
-                                .map(CompanyPersonDTO::fromEntity)
-                                .collect(Collectors.toList())))
-                .collect(Collectors.toList());
-    }
+                                                companyPersonRepo.findByCompanyUuid(cc.getUuid())
+                                                                .stream()
+                                                                .map(CompanyPersonDTO::fromEntity)
+                                                                .collect(Collectors.toList())))
+                                .collect(Collectors.toList());
+        }
 
-    public CustomerCompanyDTO getByUuid(String uuid) {
-        return customerCompanyRepo.findByUuid(uuid)
-                .map(cc -> new CustomerCompanyDTO(
-                        cc,
-                        companyAddressRepo.findByCompanyUuid(cc.getUuid())
-                                .stream()
-                                .map(CompanyAddressDTO::fromEntity)
-                                .collect(Collectors.toList()),
+        public CustomerCompanyDTO getByUuid(String uuid) {
+                return customerCompanyRepo.findByUuid(uuid)
+                                .map(cc -> new CustomerCompanyDTO(
+                                                cc,
+                                                companyAddressRepo.findByCompanyUuid(cc.getUuid())
+                                                                .stream()
+                                                                .map(CompanyAddressDTO::fromEntity)
+                                                                .collect(Collectors.toList()),
 
-                        companyPersonRepo.findByCompanyUuid(cc.getUuid())
-                                .stream()
-                                .map(CompanyPersonDTO::fromEntity)
-                                .collect(Collectors.toList())))
-                .orElse(null);
-    }
+                                                companyPersonRepo.findByCompanyUuid(cc.getUuid())
+                                                                .stream()
+                                                                .map(CompanyPersonDTO::fromEntity)
+                                                                .collect(Collectors.toList())))
+                                .orElse(null);
+        }
 }
