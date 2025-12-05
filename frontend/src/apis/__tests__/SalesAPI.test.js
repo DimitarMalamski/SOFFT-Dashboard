f7b383772d82d15e04bb01740f0ea348c9fa35c9
DustVeer <dveerdonk@gmail.com>
2025-12-05 15:48:39 +0100

    Added environment  variables to the frontend
    Make sure to add the .env file to you local files manually, because it wil not be pulled

--- frontend/src/apis/__tests__/SalesAPI.test.js
+++ frontend/src/apis/__tests__/SalesAPI.test.js
@@ -1,6 +1,9 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import axios from "axios";
 import SalesAPI from "../SalesAPI.js";
+import.meta.env.VITE_API_URL;
+
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 vi.mock("axios");
 
@@ -21,7 +24,7 @@ describe("SalesAPI", () => {
         expect(result).toEqual([{ id: 1 }, { id: 2 }]);
 
         expect(axios.get).toHaveBeenCalledWith(
-            "http://localhost:8080/api/salesoffers/sales?statuses=Approved&statuses=Completed"
+            `${BASE_URL}/api/salesoffers/sales?statuses=Approved&statuses=Completed`
         );
     });
 
@@ -34,7 +37,7 @@ describe("SalesAPI", () => {
         expect(result).toEqual([{ id: 99 }]);
 
         expect(axios.get).toHaveBeenCalledWith(
-            "http://localhost:8080/api/salesoffers/sales?statuses=Pending&statuses=Declined"
+            `${BASE_URL}/api/salesoffers/sales?statuses=Pending&statuses=Declined`
         );
     });
 
