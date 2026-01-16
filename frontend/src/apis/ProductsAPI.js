26f13210805a61f77a62f653ab22afb853a0e7fe
DustVeer <dveerdonk@gmail.com>
2026-01-16 11:10:01 +0100

    changed ProductsAPI to use .env

--- frontend/src/apis/ProductsAPI.js
+++ frontend/src/apis/ProductsAPI.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api/products";
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+const PRODUCTS_BASE_URL = `${BASE_URL}/api/salesoffers/products`;
 
 const cleanParams = (params) => {
     const cleaned = {};
@@ -13,12 +15,12 @@ const cleanParams = (params) => {
 
 const ProductsAPI = {
     getPaged: ({ page = 0, size = 16, name, classType, color }) =>
-        axios.get(`${BASE_URL}/paged`, {
+        axios.get(`${PRODUCTS_BASE_URL}/paged`, {
             params: cleanParams({ page, size, name, class: classType, color })
         }).then(res => res.data),
 
     getById: (id) =>
-        axios.get(`${BASE_URL}/${id}`).then(res => res.data),
+        axios.get(`${PRODUCTS_BASE_URL}/${id}`).then(res => res.data),
 };
 
 export default ProductsAPI;
